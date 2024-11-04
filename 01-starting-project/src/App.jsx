e36f83798b63c4ea57635ea0b612fc2da4d0d5a5
Justin Kuan <justin.kuan@ttt.studio>
2024-11-03 20:18:41 -0800

    EOD commit. Finished section 3 of udemy course

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,59 +1,63 @@
-import reactImg from "./assets/react-core-concepts.png";
-import componentImg from "./assets/components.png";
-// import propsImg from "./assets/props.png";
-// import stateImg from "./assets/state.png";
-// import hooksImg from "./assets/hooks.png";
-
-const reactDescriptions = ["Fundamental", "Crucial", "Essential"];
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const description =
-    reactDescriptions[getRandomInt(reactDescriptions.length - 1)];
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
-function CoreConcept(props) {
-  return (
-    <li>
-      <img src={props.image} alt="..." />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
-    </li>
-  );
-}
+import { useState } from "react";
+import { CORE_CONCEPTS, EXAMPLES } from "./data";
+import Header from "./components/Header";
+import CoreConcept from "./components/CoreConcept";
+import TabButton from "./components/TabButton";
 
 function App() {
+  const [selectedTab, setSelectedTab] = useState("components");
+  function handleTabSelect(tab) {
+    setSelectedTab(tab);
+  }
   return (
     <div>
       <Header />
       <main>
+        <h2>Time to get started!</h2>
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept
-              title="Components"
-              description="The core UI building block"
-              image={componentImg}
-            />
-            <CoreConcept />
-            <CoreConcept />
-            <CoreConcept />
+            {CORE_CONCEPTS.map((coreConcept) => (
+              <CoreConcept key={coreConcept.title} {...coreConcept} />
+            ))}
           </ul>
         </section>
-        <h2>Time to get started!</h2>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton
+              isSelected={selectedTab === "componenets"}
+              onSelect={() => handleTabSelect("components")}
+            >
+              Components
+            </TabButton>
+            <TabButton
+              isSelected={selectedTab === "jsx"}
+              onSelect={() => handleTabSelect("jsx")}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              isSelected={selectedTab === "props"}
+              onSelect={() => handleTabSelect("props")}
+            >
+              Props
+            </TabButton>
+            <TabButton
+              isSelected={selectedTab === "state"}
+              onSelect={() => handleTabSelect("state")}
+            >
+              State
+            </TabButton>
+          </menu>
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTab].title}</h3>
+            <p>{EXAMPLES[selectedTab].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTab].code}</code>
+            </pre>
+          </div>
+        </section>
       </main>
     </div>
   );
